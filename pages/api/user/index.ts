fc2656c32721e078eb67fbd387471271a884df51
MohamedOMourad <mohamed.o.murad@gmail.com>
2022-09-15 18:13:37 +0200

    solve bug

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -22,20 +22,24 @@ export default async function handler(
             res.status(200).json({ user });
             break;
         case "POST":
-            const newUser = await prisma.user.create({
-                data: {
-                    firstName,
-                    lastName,
-                    email,
-                    password,
-                    gender,
-                    age,
-                    height,
-                    weight,
-                    imgUrl,
-                },
-            });
-            res.status(200).json({ newUser });
+            try {
+                const newUser = await prisma.user.create({
+                    data: {
+                        firstName,
+                        lastName,
+                        email,
+                        password,
+                        gender,
+                        age,
+                        height,
+                        weight,
+                        imgUrl,
+                    },
+                });
+                res.status(200).json({ newUser });
+            } catch (error) {
+                res.status(400)
+            }
             break;
         default:
             res.status(404).json("Not Found!");
--- utils/API.ts
+++ utils/API.ts
@@ -5,17 +5,17 @@ const API = axios.create({ baseURL: "http://localhost:3000/api" });
 
 export const createUser = async (values: any) => {
     const res = await API.post('/user', values)
-    const user = await res.data.user;
+    const user = await res.data.newUser;
     console.log(user)
 }
 
 export const loginAPI = async (values: object) => {
-  try {
-    const res = await API.get("/user",values );
-    const user = await res.data.user
-    console.log(user);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const res = await API.get("/user", values);
+        const user = await res.data.user
+        console.log(user);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
