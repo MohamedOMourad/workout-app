fe601cb6d54103aa4f0bed17291b8bfe192b5ddf
FahadAlshemali <fhadfhad222@gmail.com>
2022-09-19 12:56:39 +0200

    calender

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,7 +14,7 @@ import { useRouter } from 'next/router'
 const navigation = [
     { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
     { name: "Browse WorkOut", href: "/browseWorkout", icon: BoltIcon, current: false },
-    { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+    { name: "Calendar", href: "/calender", icon: CalendarIcon, current: false },
     { name: "Progress", href: "#", icon: ChartBarIcon, current: false },
 ];
 const userNavigation = [
--- pages/calender.tsx
+++ pages/calender.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Calender = () => {
-    return (
-        <div>
-            Calander
-        </div>
-    )
-}
+  return (
+    <div className="min-h-screen bg-gray-100 ">
+      <div className="p-4">
+        <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">Workout History</h1>
+      </div>
+      <div className="sticky top-20 bg-gray-50 px-4 py-6 shadow sm:rounded sm:p-6 md:flex md:items-center md:justify-between md:space-x-6 lg:space-x-8">
+            <div>
+                <h3>
+                    Workout
+                </h3>
+            </div>
+      </div>
+    </div>
+  );
+};
 
 export default Calender;
