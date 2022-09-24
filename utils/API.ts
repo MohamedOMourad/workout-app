8dd26e2f674bc5cdc0023c87624eb7e4e276912c
MohamedOMourad <mohamed.o.murad@gmail.com>
2022-09-24 15:19:47 +0200

    calcaulate user pest records

--- utils/API.ts
+++ utils/API.ts
@@ -4,13 +4,11 @@ export const createUser = async (values: any, id: any) => {
     console.log(id)
     const res = await axios.post('/api/user', { ...values, id: id })
     const user = await res.data.newUser;
-    console.log(user)
 }
 
 export const addUserRecord = async (weight: string, reps: string, step: number, id: string) => {
-    console.log({ workoutLineId: id, reps: reps, step: step, weight: weight })
-    const res = await axios.post('/api/userrecords', { workoutLineId: id, reps: reps, step: step, weight: weight })
+    console.log({ workoutLineId: id, reps: +reps, step: +step, weight: +weight })
+    const res = await axios.post('/api/userrecords', { workoutLineId: id, reps: +reps, step: +step, weight: +weight })
     const user = await res.data.newUser;
-    console.log(user)
 }
 
